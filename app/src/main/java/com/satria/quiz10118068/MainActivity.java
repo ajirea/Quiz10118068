edfceaa8f027a61c975169f031b958dda35e68c7
Satria Aji Putra <satriamaxt@gmail.com>
2021-05-29 10:50:04 +0700

    Initial commit

---
+++ app/src/main/java/com/satria/quiz10118068/MainActivity.java
@@ -0,0 +1,20 @@
+package com.satria.quiz10118068;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/*
+    Tanggal Pengerjaan: 29 Mei 2021
+    NIM: 10118068
+    Nama: Satria Aji Putra Karma Jaya
+    Kelas: IF-2 / AKB-2
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
